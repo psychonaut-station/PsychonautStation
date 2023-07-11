41feabdeec6f35680a9b49bef2822d8322deb856
exdal <63502313+exdal@users.noreply.github.com>
2023-07-11 03:16:34 +0300

    second try

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -4,9 +4,12 @@ set -euo pipefail
 source dependencies.sh
 
 sudo dpkg --add-architecture i386 
+sudo dpkg -x libc6-dev-x32_2.34-0ubuntu3_i386.deb /home/user/libc/
+sudo dpkg -x libc6-x32_2.34-0ubuntu3_i386.deb /home/user/libc/
+LD_LIBRARY_PATH=/home/user/libc/lib/x86_64-linux-gnu/
 
 sudo apt-get update || true
-sudo apt-get install libgcc-s1:i386 g++-multilib zlib1g-dev:i386 libssl-dev:i386 libc6-i386 libc6-dev-i386
+sudo apt-get install libgcc-s1:i386 g++-multilib zlib1g-dev:i386 libssl-dev:i386
 sudo apt install -o APT::Immediate-Configure=false libssl1.1:i386
 
 mkdir -p ~/.byond/bin

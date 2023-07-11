22a9eb446e13c01cdd85528e67b38b30cc819fa6
exdal <63502313+exdal@users.noreply.github.com>
2023-07-11 03:37:16 +0300

    you need to be linked first

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -9,7 +9,7 @@ sudo wget https://launchpad.net/ubuntu/+source/glibc/2.34-0ubuntu3/+build/222281
 sudo dpkg -x libc6-dev_2.34-0ubuntu3_i386.deb ~/libc/
 sudo wget https://launchpad.net/ubuntu/+source/glibc/2.34-0ubuntu3/+build/22228180/+files/libc6_2.34-0ubuntu3_i386.deb
 sudo dpkg -x libc6_2.34-0ubuntu3_i386.deb ~/libc/
-export LD_LIBRARY_PATH=~/libc/usr/lib/i386-linux-gnu/${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=~/libc/usr/lib/i386-linux-gnu/
 
 sudo apt-get update || true
 sudo apt-get install libgcc-s1:i386 g++-multilib zlib1g-dev:i386 libssl-dev:i386

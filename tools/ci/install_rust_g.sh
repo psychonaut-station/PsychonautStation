c7c9b2879f81d594f9de344ec8875cb9f1a389c1
exdal <63502313+exdal@users.noreply.github.com>
2023-07-11 03:29:04 +0300

    okay

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -4,11 +4,12 @@ set -euo pipefail
 source dependencies.sh
 
 sudo dpkg --add-architecture i386 
+mkdir libc
 sudo wget https://launchpad.net/ubuntu/+source/glibc/2.34-0ubuntu3/+build/22228180/+files/libc6-dev_2.34-0ubuntu3_i386.deb
-sudo dpkg -x libc6-dev-x32_2.34-0ubuntu3_i386.deb /home/user/libc/
+sudo dpkg -x libc6-dev_2.34-0ubuntu3_i386.deb ~/libc/
 sudo wget https://launchpad.net/ubuntu/+source/glibc/2.34-0ubuntu3/+build/22228180/+files/libc6_2.34-0ubuntu3_i386.deb
-sudo dpkg -x libc6-x32_2.34-0ubuntu3_i386.deb /home/user/libc/
-LD_LIBRARY_PATH=/home/user/libc/lib/x86_64-linux-gnu/
+sudo dpkg -x libc6-x32_2.34-0ubuntu3_i386.deb ~/libc/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/libc/usr/lib/i386-linux-gnu/
 
 sudo apt-get update || true
 sudo apt-get install libgcc-s1:i386 g++-multilib zlib1g-dev:i386 libssl-dev:i386

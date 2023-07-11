0d2d78bb96c13aa574e5315a1636f3aeaf70e817
exdal <63502313+exdal@users.noreply.github.com>
2023-07-11 03:02:45 +0300

    holy shit

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -11,12 +11,12 @@ sudo apt-get install gawk bison -y
 wget -c https://ftp.gnu.org/gnu/glibc/glibc-2.34.tar.gz
 tar -zxvf glibc-2.34.tar.gz && cd glibc-2.34
 mkdir glibc-build && cd glibc-build
-../configure --prefix=/opt/glibc-2.34
+../configure --prefix=/usr --disable-profile --enable-add-ons --with-headers=/usr/include
 make 
 sudo make install
 
 sudo apt-get update || true
-sudo apt-get install libgcc-s1:i386 g++-multilib zlib1g-dev:i386 libssl-dev:i386 libglib2.0-0 libglib2.0-dev
+sudo apt-get install libgcc-s1:i386 g++-multilib zlib1g-dev:i386 libssl-dev:i386
 sudo apt install -o APT::Immediate-Configure=false libssl1.1:i386
 
 mkdir -p ~/.byond/bin

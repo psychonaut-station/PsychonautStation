eb7009142c056e7c728361db2e37fdc886becb7a
genku <koraygench@gmail.com>
2024-03-14 00:14:20 +0300

    update | Yasalar ekranının arayüzü geliştirildi.

---
+++ tgui/packages/tgui/interfaces/Laws/LawDesc.jsx
@@ -0,0 +1,115 @@
+// @Genku
+
+// Türkçeleştirmeye açıktır.
+// Title kısmındaki isimler ile Table dosyasındaki
+// isimlerin aynı olmasına dikkat edin.
+
+export const lawDesc = [{
+  title: "Disturbing the Peace",
+  desc: "To disturb the station's or crew's peace.",
+}, {
+  title: "Assault",
+  desc: "To use physical force against someone without the apparent intent to kill them.",
+}, {
+  title: "Assault With a Deadly Weapon",
+  desc: "To use physical force, through a deadly weapon, against someone without the apparent intent to kill them.",
+}, {
+  title: "Attempted Murder",
+  desc: "To use physical force against a person after that person is in a critical state with the apparent intent to kill them.",
+}, {
+  title: "Murder",
+  desc: "To maliciously kill someone.",
+}, {
+  title: "Animal Cruelty",
+  desc: "To harm or kill animals.",
+}, {
+  title: "Assault of an Officer",
+  desc: "To use physical force against a Department Head or member of Security without the apparent intent to kill them.",
+}, {
+  title: "Spreading Disinformation",
+  desc: "To maliciously spread disinformation among the crew.",
+}, {
+  title: "Rioting",
+  desc: "To partake in an unauthorized and disruptive assembly of crewmen that refuse to disperse.",
+}, {
+  title: "Inciting a Riot",
+  desc: "To attempt to stir the crew into a riot",
+}, {
+  title: "Brainwashing",
+  desc: "To brainwash or enslave people against their will.",
+}, {
+  title: "Mutiny",
+  desc: "To act individually, or as a group, to overthrow or subvert the established Chain of Command without lawful and legitimate cause.",
+}, {
+  title: "Littering",
+  desc: "To litter the station interior with trash, blood, gibs, oil, vomit, and other rubbish.",
+}, {
+  title: "Vandalism",
+  desc: "To deliberately damage the station with malicious intent.",
+}, {
+  title: "Sabotage",
+  desc: "To hinder the work of the crew or station through malicious actions.",
+}, {
+  title: "Grand Sabotage",
+  desc: "To engage in maliciously destructive actions, seriously threatening crew or station.",
+}, {
+  title: "Insubordination",
+  desc: "To disobey a lawful direct order from one's superior officer.",
+}, {
+  title: "Dereliction of Duty",
+  desc: "To willfully abandon an obligation that is critical to the station's continued operation.",
+}, {
+  title: "Aiding and Abetting",
+  desc: "To knowingly assist a criminal, including by interfering with a lawful arrest.",
+}, {
+  title: "Enemy of the Corporation",
+  desc: "To act as, or knowingly aid, an enemy of Nanotrasen.",
+}, {
+  title: "Trespass",
+  desc: "To be in an area which a person does not have access to. This counts for general areas of the ship, and trespass in restricted areas is a more serious crime.",
+}, {
+  title: "Breaking and Entry",
+  desc: "Forced entry to areas where the subject does not have access to. This counts for general areas, and breaking into restricted areas is a more serious crime.",
+}, {
+  title: "B&E of a Restricted Area",
+  desc: "This is breaking into any Security area, Command area (Bridge, Captains Quarters, Teleporter, etc.), the Engine Room, Atmos, or Toxins research.",
+}, {
+  title: "High-Security Trespass",
+  desc: "Being in a restricted area without prior authorisation. This includes any Security Area, Command area (including EVA), the Engine Room, Atmos, or Toxins Research.",
+}, {
+  title: "Possession of a Weapon",
+  desc: "To be in possession of a dangerous item that is not part of their job role.",
+}, {
+  title: "Possession of a Restricted Weapon",
+  desc: "To be in possession of a restricted weapon without prior authorisation, such as: Guns, Batons, Flashes, Grenades, etc.",
+}, {
+  title: "Contraband",
+  desc: "To possess highly dangerous contraband items.	",
+}, {
+  title: "Drug Possession",
+  desc: "To possess space drugs or other narcotics by unauthorized personnel.",
+}, {
+  title: "Possession of Explosives",
+  desc: "To be in possession of an explosive device.",
+}, {
+  title: "Petty Theft",
+  desc: "To take items from a person, or an area one does not have access to or to take items belonging to the station as a whole.",
+}, {
+  title: "Theft",
+  desc: "To steal restricted or dangerous items",
+}, {
+  title: "Grand Theft",
+  desc: "To steal items of high value or sensitive nature.",
+}, {
+  title: "Kidnapping",
+  desc: "To kidnap a person or hold them hostage.",
+}, {
+  title: "Indecent Exposure",
+  desc: "To be intentionally and publicly unclothed.",
+}, {
+  title: "Creating a Workplace Hazard",
+  desc: "To endanger the crew or station through negligent or irresponsible, but not deliberately malicious, actions.",
+}, {
+  title: "Manslaughter",
+  desc: "To unintentionally kill someone through negligent, but not malicious, actions.",
+}];
---
+++ tgui/packages/tgui/interfaces/Laws/LawTable.jsx
@@ -0,0 +1,91 @@
+// @Genku
+
+// Türkçeleştirmeye açıktır.
+// Title kısmındaki isimler ile Table dosyasındaki
+// isimlerin aynı olmasına dikkat edin.
+
+export const lawList = [{
+  code: "X01",
+  Mischief: "Disturbing the Peace",
+  Misdemeanor: "Assault",
+  Felony: "Assault With a Deadly Weapon",
+  GrandFelony: "Attempted Murder",
+  CapitalCrime: "Murder",
+}, {
+  code: "X02",
+  Mischief: " ",
+  Misdemeanor: "Animal Cruelty",
+  Felony: "Assault of an Officer",
+  GrandFelony: " ",
+  CapitalCrime: " ",
+}, {
+  code: "X03",
+  Mischief: "Spreading Disinformation",
+  Misdemeanor: "Rioting",
+  Felony: "Inciting a Riot",
+  GrandFelony: "Brainwashing",
+  CapitalCrime: "Mutiny",
+}, {
+  code: "X04",
+  Mischief: "Littering",
+  Misdemeanor: "Vandalism",
+  Felony: "Sabotage",
+  GrandFelony: "Grand Sabotage",
+  CapitalCrime: " ",
+}, {
+  code: "X05",
+  Mischief: "Insubordination",
+  Misdemeanor: "Dereliction of Duty",
+  Felony: "Aiding and Abetting",
+  GrandFelony: "Enemy of the Corporation",
+  CapitalCrime: " ",
+}, {
+  code: "X06",
+  Mischief: "Trespass",
+  Misdemeanor: "Breaking and Entry",
+  Felony: "B&E of a Restricted Area",
+  GrandFelony: " ",
+  CapitalCrime: " ",
+}, {
+  code: "X07",
+  Mischief: " ",
+  Misdemeanor: " ",
+  Felony: "High-Security Trespass",
+  GrandFelony: " ",
+  CapitalCrime: " ",
+}, {
+  code: "X08",
+  Mischief: "Possession of a Weapon",
+  Misdemeanor: "Possession of a Restricted Weapon",
+  Felony: "Contraband",
+  GrandFelony: " ",
+  CapitalCrime: " ",
+}, {
+  code: "X09",
+  Mischief: "Drug Possession",
+  Misdemeanor: " ",
+  Felony: "Possession of Explosives",
+  GrandFelony: " ",
+  CapitalCrime: " ",
+}, {
+  code: "X10",
+  Mischief: " ",
+  Misdemeanor: "Petty Theft",
+  Felony: "Theft",
+  GrandFelony: "Grand Theft",
+  CapitalCrime: " ",
+}, {
+  code: "X11",
+  Mischief: " ",
+  Misdemeanor: " ",
+  Felony: "Kidnapping",
+  GrandFelony: " ",
+  CapitalCrime: " ",
+}, {
+  code: "X12",
+  Mischief: "Indecent Exposure",
+  Misdemeanor: "Creating a Workplace Hazard",
+  Felony: "Manslaughter",
+  GrandFelony: " ",
+  CapitalCrime: " ",
+}];

1eda13a791844875f9178cbcdbae6b1b1ef63b5c
Rengan <takibrine07@gmail.com>
2025-02-21 18:51:48 +0300

    Ğ

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/face_cursor_combat_mode.tsx
@@ -0,0 +1,10 @@
+import { CheckboxInput, FeatureToggle } from '../base';
+
+export const face_cursor_combat_mode: FeatureToggle = {
+  name: 'Face cursor with combat mode',
+  category: 'GAMEPLAY',
+  description: `
+    When toggled, you will now face towards the cursor with combat mode enabled.
+  `,
+  component: CheckboxInput,
+};

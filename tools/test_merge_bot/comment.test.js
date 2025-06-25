23c5e5ecb13c4210c0155985846b438216ee16d5
RengaN02 <60517664+RengaN02@users.noreply.github.com>
2025-06-25 19:17:46 +0300

    mirror conflict fix part 2 (#503)
    Co-authored-by: Sefa <50076109+Seefaaa@users.noreply.github.com>

--- tools/test_merge_bot/comment.test.js
+++ tools/test_merge_bot/comment.test.js
@@ -1,34 +1,14 @@
 import { strict as assert } from "assert";
 import { createComment } from "./comment.js";
 
-<<<<<<< HEAD
 const baseRounds = [
-	{
-		round_id: 1,
-		datetime: "2020-01-01 00:00:00",
-	},
+  {
+    round_id: 1,
+    datetime: "2020-01-01 00:00:00",
+  },
 ];
 
-const baseComment = createComment(
-	baseRounds,
-=======
-const baseServers = {
-  bagil: [
-    {
-      round_id: 1,
-      datetime: "2020-01-01 00:00:00",
-      server: "bagil",
-      url: "https://tgstation13.org/round/1",
-    },
-  ],
-};
-
-const baseComment = createComment(
-  baseServers,
->>>>>>> 87b3788cbbcea797949e856cfed8f64d65238592
-
-  null,
-);
+const baseComment = createComment(baseRounds, null);
 
 assert.equal(
   baseComment,
@@ -40,39 +20,22 @@ This pull request was test merged in 1 round(s).
 - 1 @ 2020-01-01 00:00:00
 
 </details>
-`,
+`
 );
 
 assert.equal(createComment(baseRounds, baseComment), null);
 
 assert.equal(
-<<<<<<< HEAD
-	createComment(
-		[
-			{
-				round_id: 2,
-				datetime: "2020-01-01 01:00:00",
-			},
-		],
-		baseComment
-	),
-	`<!-- test_merge_bot: 1, 2 -->
-=======
   createComment(
-    {
-      bagil: [
-        {
-          round_id: 2,
-          datetime: "2020-01-01 01:00:00",
-          server: "bagil",
-          url: "https://tgstation13.org/round/2",
-        },
-      ],
-    },
-    baseComment,
+    [
+      {
+        round_id: 2,
+        datetime: "2020-01-01 01:00:00",
+      },
+    ],
+    baseComment
   ),
   `<!-- test_merge_bot: 1, 2 -->
->>>>>>> 87b3788cbbcea797949e856cfed8f64d65238592
 This pull request was test merged in 2 round(s).
 <details><summary>Round list</summary>
 
@@ -81,5 +44,5 @@ This pull request was test merged in 2 round(s).
 - 1 @ 2020-01-01 00:00:00
 
 </details>
-`,
+`
 );
